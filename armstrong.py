97b8a329ac8729bb97995e3014366dec44f92674
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-11-27 14:34:14 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,14 +1,18 @@
-def armstrong_no(x):
+#Title:Armstrong Number
+#Name:Yashpal Kondgule M 31
+
+
+def armstrong_no(x): #defining a function 
  s=0
  z=x
- while x>0:
-   y=x%10
+ while x>0:  # while loop
+   y=x%10    # % gives a remainder 
    s+=y**3
-   x=x//10
+   x=x//10    # // gives quotient
  print(s)  
  if s==z:
   print("armstrong number")
- else:
+ else:                                # exits while loop
   print("not an armstrong number") 
 
 
