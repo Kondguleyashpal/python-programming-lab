fa87e877d904292c8051469718af4d1ba51f3803
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-11-27 14:58:59 +0530

    Add files via upload

---
+++ fibonocci.py
@@ -0,0 +1,26 @@
+ #Title: Fibonacci sequence 
+#Name: Yashpal Kondgule M 31
+# change this value for a different result
+nterms = 10
+
+# comment to take input from the user
+#nterms = int(input("How many terms? "))
+
+# first two terms
+n1 = 0
+n2 = 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print(n1)
+else:
+   while count < nterms:
+       print(n1,end=' , ')
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+count += 1
