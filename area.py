d5798e508a77033a165e7d54706f19ddf437b749
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-11-27 14:29:16 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,4 +1,6 @@
-def rectangle (m,n):
+### Title:To 
+#Name:Yashpal Kondgule M 31
+def rectangle (m,n): # defining a function with two parameters
  for i in range (m):
   for j in range(n):
    print("*",end=" ")
