d3ffd4ed11c88b16a7436c07806e28c750a41512
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-11-27 14:41:50 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,8 @@
-year =int(input("enter year:"))
+# Title: To check whether a year is leap year or not 
+#### Name:Yashpal Kondgule M 31
+
+
+year =int(input("enter year:"))   ### taking the input
   # leap year check 
 if  year%4==0 and year%100!=0:
      print(year, "is a leap year")
