1756ec8bbad8fe6847145c29fb8fa21f45d1fe8d
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-11-27 14:51:37 +0530

    Update root.py

--- root.py
+++ root.py
@@ -1,6 +1,8 @@
-def root (x,n):
+# Title: To find the root of a given number
+## Name: Yashpal Kondgule M 31
+def root (x,n):    ### defining a function 
   print (x**(1/n))
-x=int(input("enter no."))
+x=int(input("enter no."))   ## user input
 n=2
 root (x,n)
  
