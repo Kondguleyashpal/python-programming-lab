7e9dec3cb529ac6725ff6fc25347b6762580ce4e
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-11-27 15:06:30 +0530

    Delete area.py

--- area.py
+++
@@ -1,8 +0,0 @@
-### Title:To 
-#Name:Yashpal Kondgule M 31
-def rectangle (m,n): # defining a function with two parameters
- for i in range (m):
-  for j in range(n):
-   print("*",end=" ")
-  print()
-rectangle(2,4) 
