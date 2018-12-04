fe11e870230af07eefe500d77a32275645734290
KONDGULE YASHPAL VILASKUMAR <43171799+Kondguleyashpal@users.noreply.github.com>
2018-12-04 10:27:58 +0530

    Update functionfactorial.py

--- functionfactorial.py
+++ functionfactorial.py
@@ -1,10 +1,17 @@
-def factorial(x):
+#Title Factorial 
+# Name Yashpal Kondgule M 31
+
+
+
+
+def factorial(x): # defininf a function 
   n=0
   result=1
-  while n<x:
+  while n<x:. # while loop 
    n=n+1
    result=result*(n)
   print(result)
-x=int(input("enter no"))
+
+ x=int(input("enter no")). # user input
 z=factorial(x)
 print(z)
